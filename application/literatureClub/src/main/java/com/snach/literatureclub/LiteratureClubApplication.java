1938c1620e24f479b4b6305aaf2e5d2bcdb4e742
Mizuiro_ <1170789725@qq.com>
2023-09-14 13:35:24 +0800

    Springboot init

---
+++ application/literatureClub/src/main/java/com/snach/literatureclub/LiteratureClubApplication.java
@@ -0,0 +1,13 @@
+package com.snach.literatureclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteratureClubApplication.class, args);
+    }
+
+}
