232342051194109ab058378f54725e4e7ab06a61
Mizuiro_ <674.zfer@gmail.com>
2023-09-14 13:35:24 +0800

    Springboot init

---
+++ application/literatureClub/src/test/java/com/snach/literatureclub/LiteratureClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snach.literatureclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteratureClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
