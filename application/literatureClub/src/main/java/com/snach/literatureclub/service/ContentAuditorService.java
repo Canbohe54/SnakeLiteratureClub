0f67a352898e4cbd18f9e464e66b9ec64359345e
mky <1748438065@qq.com>
2023-09-20 10:38:43 +0800

    Add daos and services of Comment,ContentAuditor,QualificationAuditor

---
+++ application/literatureClub/src/main/java/com/snach/literatureclub/service/ContentAuditorService.java
@@ -0,0 +1,15 @@
+package com.snach.literatureclub.service;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ContentAuditorService {
+
+}
+
+@Service
+@Mapper
+class ContentAuditorImpl implements ContentAuditorService{
+
+}
---
+++ application/literatureClub/src/main/java/com/snach/literatureclub/service/ExpertService.java
@@ -0,0 +1,12 @@
+package com.snach.literatureclub.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ExpertService {
+}
+
+@Service
+class ExpertServiceImpl implements ExpertService{
+
+}
