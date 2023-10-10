4214cae63f413735ecc39ae063f448b024d07fa4
mky <1748438065@qq.com>
2023-10-10 20:56:41 +0800

    add article interface

--- application/literatureClub/src/main/java/com/snach/literatureclub/bean/Article.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/bean/Article.java
@@ -3,12 +3,15 @@ package com.snach.literatureclub.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Article extends BaseText implements Serializable {
     String title;
+    String description;
 }
