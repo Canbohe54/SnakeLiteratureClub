e21d9f2915beba37a51c71020a83d07c9a7cf1b1
mky <1748438065@qq.com>
2023-10-11 15:47:10 +0800

    update article module 1 and change type od BasicText.id.

--- application/literatureClub/src/main/java/com/snach/literatureclub/bean/Article.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/bean/Article.java
@@ -7,11 +7,16 @@ import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 import java.math.BigInteger;
+import java.util.Date;
 
 @Data
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Article extends BaseText implements Serializable {
     String title;
     String description;
+    public Article(String id, String text, Date time,String textBy, String title, String description){
+        super(id,text,time,textBy);
+        this.title = title;
+        this.description = description;
+    }
 }
