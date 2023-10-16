6899a17b712f5c98d1b5a92474b0898204307e6e
Canbohe54 <Canbohe54@outlook.com>
2023-10-16 11:35:17 +0800

    update gitignore

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import App from './App.vue'
-import router from './router'
-
-const Vue = createApp(App)
-Vue.use(router)
-Vue.use(ElementPlus)
-Vue.mount('#app')
