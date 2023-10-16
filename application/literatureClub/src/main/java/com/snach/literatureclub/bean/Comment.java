9e5a07aa931b14037dedadc5ef9db96aa1204e1a
Mizuiro_ <1170789725@qq.com>
2023-10-16 20:07:52 +0800

    package init

--- application/literatureClub/src/main/java/com/snach/literatureclub/bean/Comment.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/bean/Comment.java
@@ -6,9 +6,24 @@ import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
+/**
+ * Attributes:
+ *     textOn(String): BaseText.id, Comment to whom(a ROOT Comment or an Article)
+ *     reply (String): if `textOn` is a Comment, `reply` is the Comment that it actually replies to
+ *     <p>-----------------Example-----------------</p>
+ *     article
+ *        |--> rootComment{textOn=article.id, reply=null}
+ *        |        |--> subComment_1 {textOn=rootComment.id, reply=comment_1.id}
+ *        |        |       |--> comment_3 {textOn=rootComment.id, reply=comment_2.id}
+ *        |        |--> subComment_2 {textOn=rootComment.id, reply=comment_1.id}
+ *        |--> ...
+ */
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Comment extends BaseText implements Serializable {
-    String textTo;
+
+    String textOn;
+
+    String reply;
 }
--- application/literatureClub/src/main/java/com/snach/literatureclub/service/CommentService.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/service/CommentService.java
@@ -1,17 +1,86 @@
 package com.snach.literatureclub.service;
 
+import com.snach.literatureclub.bean.Comment;
+import com.snach.literatureclub.dao.CommentDao;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static com.snach.literatureclub.utils.Tools.*;
 
 @Service
 public interface CommentService {
+    /**
+     * 根据稿件id载入稿件下评论
+     *
+     * @param articleId 稿件id
+     * @param recursive 是否使用递归载入评论（即包含子评论）
+     * @return
+     * <p>
+     * 关于`根评论`及`子评论`:
+     * @see Comment
+     */
+    Map<String, Object> loadComment(String articleId, Boolean recursive);
 
+    /**
+     * 添加评论
+     *
+     * @param commentTo 稿件id或评论id
+     */
+    Map<String, Object> addComment(String commentTo);
+
+    /**
+     * 删除评论
+     *
+     * @param commentId 评论id
+     * @param reclusive 是否递归删除所有评论，默认false
+     */
+    void deleteComment(String commentId, Boolean reclusive);
 }
 
 @Service
 @Mapper
-class CommentServiceImpl implements CommentService{
+class CommentServiceImpl implements CommentService {
+    @Autowired
+    private CommentDao commentDao;
 
+    @Override
+    public Map<String, Object> loadComment(String articleId, Boolean recursive) {
+        Map<String, Object> response = new HashMap<>();
+        if (idType(articleId) == Type.ARTICLE) {
+            response.put("statusMsg", "Invalid id.");
+            response.put("res", "{}");
+            return response;
+        }
+        List<Object> resultList = new ArrayList<>();
+        List<Comment> onArticle = commentDao.loadComment(articleId);
+        resultList.add(onArticle);
+        if (recursive) {
+            List<List<Comment>> onComment = new ArrayList<>();
+            for (Comment rootComment : onArticle) {
+                onComment.add(commentDao.loadComment(rootComment.getId()));
+            }
+            resultList.add(onComment);
+        }
+        response.put("statusMsg", "Success.");
+        response.put("res", resultList);
+        return response;
+    }
+
+    @Override
+    public Map<String, Object> addComment(String commentTo) {
+        Map<String, Object> response = new HashMap<>();
+//        commentDao.insertComment(commentTo);
+        return response;
+    }
+
+    @Override
+    public void deleteComment(String commentId, Boolean reclusive) {
+        commentDao.deleteComment(commentId);
+    }
 }
