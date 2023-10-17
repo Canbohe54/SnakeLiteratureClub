151b36b8ebb7c1f76058bc5cb697478a9bbf93ae
Canbohe54 <Canbohe54@outlook.com>
2023-10-17 21:06:20 +0800

    update login view

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -1,5 +1,32 @@
 <template>
+  <logo-title />
   <div class="login">
-    <h1>This is an login page</h1>
+    <h1>登录</h1>
+    <LoginForm />
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import LogoTitle from '../components/LogoTitle.vue'
+import LoginForm from '../components/LoginForm.vue'
+@Options({
+  components: {
+    LoginForm,
+    LogoTitle
+  }
+})
+export default class LoginView extends Vue {}
+</script>
+<style>
+.login {
+  text-align: center;
+  background-color: #fafafa;
+  border-radius: 20px;
+  width: 460px;
+  position: absolute;
+  padding: 30px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+</style>
