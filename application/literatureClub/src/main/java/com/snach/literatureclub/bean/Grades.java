cecfa5c29e13c762bb03c099cf63df734f4015b0
Canbohe54 <canbohe54@outlook.com>
2023-10-18 10:45:52 +0800

    fix logo shift problem; add userInfo redirect link

---
+++ application/literatureClub/src/main/java/com/snach/literatureclub/bean/Grades.java
@@ -0,0 +1,20 @@
+package com.snach.literatureclub.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Grades implements Serializable {
+    String expert_id;
+    String article_id;
+    int garde_expr;
+    int grade_struct;
+    int grade_theme;
+    int grade_all;
+    String advice;
+}
