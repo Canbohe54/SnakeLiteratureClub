a91be100ee8b3858c11e35bfb8b3e0d5937f394c
Mizuiro_ <674.zfer@gmail.com>
2023-11-14 22:41:28 +0800

    update sync axios request

--- web/src/scripts/Axios.ts
+++ web/src/scripts/Axios.ts
@@ -1,12 +1,12 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, {AxiosResponse} from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:19198'
 
-export function GET (url: string, params: object = {}, onReady?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
+export function GET(url: string, params: object = {}, onReady?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
   axios({
     url: url,
     method: 'GET',
-    headers: { 'Content-Type': 'multipart/form-data' },
+    headers: {'Content-Type': 'multipart/form-data'},
     params: params
   }).then(function (response: AxiosResponse) {
     onReady?.(response)
@@ -15,15 +15,45 @@ export function GET (url: string, params: object = {}, onReady?: (response: Axio
   })
 }
 
-export function POST (url: string, data: object = {}, callback?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
+export function POST(url: string, data: object = {}, onReady?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
   axios({
     url: url,
     method: 'POST',
-    headers: { 'Content-Type': 'multipart/form-data' },
+    headers: {'Content-Type': 'multipart/form-data'},
     data: data
   }).then(function (response: AxiosResponse) {
-    callback?.(response)
+    onReady?.(response)
   }).catch(function (error: any) {
-    console.log(error)
+    onError?.(error)
   })
+}
+
+export async function SYNC_GET(url: string, params: object = {}, onReady?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
+  await (
+    axios({
+      url: url,
+      method: 'GET',
+      headers: {'Content-Type': 'multipart/form-data'},
+      params: params
+    }).then(function (response: AxiosResponse) {
+      onReady?.(response)
+    }).catch(function (error: any) {
+      onError?.(error)
+    })
+  )
+}
+
+export async function SYNC_POST(url: string, data: object = {}, onReady?: (response: AxiosResponse) => void, onError?: (error?: any) => void) {
+  await (
+    axios({
+      url: url,
+      method: 'POST',
+      headers: {'Content-Type': 'multipart/form-data'},
+      data: data
+    }).then(function (response: AxiosResponse) {
+      onReady?.(response)
+    }).catch(function (error: any) {
+      onError?.(error)
+    })
+  )
 }
