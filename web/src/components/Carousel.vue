4ff2b23731086a390896cf2621b20c1f956f5ef7
mky <1748438065@qq.com>
2023-12-25 20:54:53 +0800

    Add carousel to Lobby.vue。

---
+++ web/src/components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-carousel :interval="4000" type="card">
+    <el-carousel-item v-for="item in imageList" :key="item">
+      <img :src="item.url" no-repeat style="object-fit: fill;height:100%; width:100%;" :alt="item.description"/>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+<script lang="ts" setup>
+// import { getAssetsFile } from "@/scripts/ImageUtil";
+import snake from '@/assets/snake.png'
+const imageList = [
+  { url: snake , description: 'snakeliteratureclub'},
+  { url: snake, description: 'snake'},
+  { url: snake, description: 'snake' },
+];
+
+</script>
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+
+
--- web/src/views/LobbyView.vue
+++ web/src/views/LobbyView.vue
@@ -2,6 +2,7 @@
   <div class="lobby">
     <NavBar class="navbar"/><!--导航栏-->
     <h1>This is Lobby Page</h1>
+    <Carousel/>
   </div>
 </template>
 <script lang="ts">
@@ -9,11 +10,14 @@ import { Options, Vue } from 'vue-class-component'
 import NavBar from '../components/NavBar.vue'
 import infroll from '../components/temp_infroll.vue'
 import { useStore } from 'vuex'
+import Carousel from "../components/Carousel.vue";
+
 console.log(useStore())
 @Options({
   components: {
     NavBar,
-    infroll
+    infroll,
+    Carousel
   }
 })
 export default class LobbyView extends Vue {}
