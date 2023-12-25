0a2ac8fdd4c2a97b742848dea1ecb9f2676395c2
mky <1748438065@qq.com>
2023-12-26 01:07:44 +0800

    Add lobbyArticle to Lobby.vue。

--- web/src/views/LobbyView.vue
+++ web/src/views/LobbyView.vue
@@ -3,6 +3,7 @@
     <NavBar class="navbar"/><!--导航栏-->
     <h1>This is Lobby Page</h1>
     <Carousel/>
+    <LobbyArticle/>
   </div>
 </template>
 <script lang="ts">
@@ -11,13 +12,15 @@ import NavBar from '../components/NavBar.vue'
 import infroll from '../components/temp_infroll.vue'
 import { useStore } from 'vuex'
 import Carousel from "../components/Carousel.vue";
+import LobbyArticle from "@/components/LobbyArticle.vue";
 
 console.log(useStore())
 @Options({
   components: {
     NavBar,
     infroll,
-    Carousel
+    Carousel,
+    LobbyArticle
   }
 })
 export default class LobbyView extends Vue {}
