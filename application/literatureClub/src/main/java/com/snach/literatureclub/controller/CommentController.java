457ed9ad691d0725a7f054b5a4460a645d190b83
Mizuiro_ <674.zfer@gmail.com>
2023-12-27 23:13:49 +0800

    comment function added

--- application/literatureClub/src/main/java/com/snach/literatureclub/controller/CommentController.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/controller/CommentController.java
@@ -16,23 +16,25 @@ public class CommentController {
     @RequestMapping(value = "load", method = RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> loadComment(@RequestParam("id") String id,
-                                           @RequestParam(value = "recursive", defaultValue = "false") Boolean recursive) {
-        return commentService.loadComment(id, recursive);
+                                           @RequestParam(value = "recursive", defaultValue = "false") Boolean recursive,
+                                           @RequestParam(value = "startAt", defaultValue = "0") int startAt,
+                                           @RequestParam(value = "limit", defaultValue = "10") int limit) {
+        return commentService.loadComment(id, recursive, startAt, limit);
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     @ResponseBody
-    public void addComment(@RequestParam("token") String token,
+    public Map<String, Object> addComment(@RequestParam("token") String token,
                            @RequestParam("text") String text,
                            @RequestParam("textOn") String textOn,
                            @RequestParam(value = "reply", required = false) String reply) {
-        commentService.addComment(token, text, textOn, reply);
+        return commentService.addComment(token, text, textOn, reply);
     }
 
     @RequestMapping(value = "del", method = RequestMethod.POST)
     @ResponseBody
-    public void deleteComment(@RequestParam("token") String token,
+    public Map<String, Object> deleteComment(@RequestParam("token") String token,
                               @RequestParam("id") String id) {
-        commentService.deleteComment(token, id);
+        return commentService.deleteComment(token, id);
     }
 }
