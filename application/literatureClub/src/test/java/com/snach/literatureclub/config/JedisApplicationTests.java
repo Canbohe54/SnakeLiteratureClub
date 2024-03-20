83942898feb76c3852fd9632604031c151fcd0e7
mky <1748438065@qq.com>
2024-03-20 16:01:21 +0800

    Add JedisApplicationTests.

---
+++ application/literatureClub/src/test/java/com/snach/literatureclub/config/JedisApplicationTests.java
@@ -0,0 +1,25 @@
+package com.snach.literatureclub.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Jedis;
+
+@SpringBootTest
+public class JedisApplicationTests {
+    @Autowired
+    private JedisPool jedisPool;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(jedisPool);
+        //在连接池中得到Jedis连接
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("haha", "你好");
+        jedis.set("name", "snach");
+        //关闭当前连接
+        jedis.close();
+
+    }
+}
