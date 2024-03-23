39ae969c5e92da1c6e8f7ce4e951ebc341ec5cb6
X <2060162656@qq.com>
2024-03-23 16:08:42 +0800

    新增word转pdf功能

--- application/literatureClub/src/main/java/com/snach/literatureclub/utils/MediaTypeConverter.java
+++ application/literatureClub/src/main/java/com/snach/literatureclub/utils/MediaTypeConverter.java
@@ -10,7 +10,7 @@ public class MediaTypeConverter {
     static {
         stringMediaTypeMap.put("text/plain", MediaType.TEXT_PLAIN);
         stringMediaTypeMap.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document", MediaType.APPLICATION_OCTET_STREAM);
-
+        stringMediaTypeMap.put("application/pdf", MediaType.APPLICATION_PDF);
     }
 
     static public MediaType getMediaType(String value){
