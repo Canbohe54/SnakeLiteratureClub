b855d6a71efb08f5361a519cad3b8d6bfd285243
Canbohe54 <Canbohe54@outlook.com>
2024-03-30 14:43:35 +0800

    public list

--- web/src/components/ArticleInfoList.vue
+++ web/src/components/ArticleInfoList.vue
@@ -1,10 +1,18 @@
 <template>
     <el-row>
-        <el-col :md="12" :sm="24" v-for="articleInfo in TestArticleList">
+        <el-col :md="12" :sm="24" v-for="articleInfo in articleListRef" v-if="is_card">
             <el-card class="article-el-card" body-style="padding:0;">
                 <ArticleInfo :articleInfo="articleInfo" :statusVisible="currentSettings.statusVisible"
                     :iconVisible="currentSettings.iconVisible" :tagsVisible="currentSettings.tagsVisible" />
             </el-card>
+        </el-col>
+        <el-col :span="24" v-if="!is_card" v-for="articleInfo in articleListRef">
+            <ArticleInfo :articleInfo="articleInfo" :statusVisible="currentSettings.statusVisible"
+                :iconVisible="currentSettings.iconVisible" :tagsVisible="currentSettings.tagsVisible"
+                class="not-card-info" />
+        </el-col>
+        <el-col :span="24" v-if="TestArticleList.length === 0">
+            <el-empty description="暂无文章" />
         </el-col>
     </el-row>
     <el-pagination class="article_list_pagination" @size-change="handleSizeChange" @current-change="handleCurrentChange"
@@ -15,14 +23,99 @@
 
 <script setup>
 import ArticleInfo from '@/components/article/ArticleInfo.vue';
-import { reactive, toRefs, ref } from 'vue';
+import { reactive, toRefs, ref, onMounted } from 'vue';
 import { useStore } from 'vuex';
 const props = defineProps({
     mode: {
         type: String,
         default: 'USER_OWN'
+    },
+    is_card: {
+        type: Boolean,
+        default: true
+    },
+    articleList: {
+        type: Array,
+        default: [
+            {
+                articleId: 'a3F8c0krY3df',
+                title: '关于沼气动力学的研究',
+                contributor: '田所浩二',
+                userId: '114514',
+                organization: '下北泽中学',
+                grade: '五年级',
+                mentor: '野兽先辈',
+                description: '这是一篇关于沼气动力学的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者田所浩二潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为沼气动力学的研究提供了新的思路和方法。',
+                status: 'PUBLISHED',
+                isPublic: true,
+                time: '1919年8月10日',
+                received_by: '下北泽日报',
+                tags: ['生物制沼', '沼气动力学']
+            },
+            {
+                articleId: 'a2',
+                title: '关于生物制沼的研究',
+                contributor: 'Mizuro_',
+                userId: '1919810',
+                organization: '下北泽师范大学',
+                grade: '大三',
+                mentor: 'mky',
+                description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
+                status: 'PUBLISHED',
+                time: '1919年8月10日',
+                received_by: '',
+                tags: ['生物制沼', '沼气动力学']
+            },
+            {
+                articleId: 'a3',
+                title: '关于生物制沼的研究',
+                contributor: 'Mizuro_',
+                userId: '1919810',
+                organization: '下北泽师范大学',
+                grade: '大三',
+                mentor: 'mky',
+                description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
+                status: 'FAIL_AUDITED',
+                time: '1919年8月10日',
+                received_by: '',
+                tags: ['生物制沼', '沼气动力学']
+            },
+            {
+                articleId: 'a4',
+                title: '关于生物制沼的研究',
+                contributor: 'Mizuro_',
+                userId: '1919810',
+                organization: '下北泽师范大学',
+                grade: '大三',
+                mentor: 'mky',
+                description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
+                status: 'ROUGH',
+                time: '1919年8月10日',
+                received_by: '',
+                tags: ['生物制沼', '沼气动力学']
+            },
+            {
+                articleId: 'a5',
+                title: '关于生物制沼的研究',
+                contributor: 'Mizuro_',
+                userId: '1919810',
+                organization: '下北泽师范大学',
+                grade: '大三',
+                mentor: 'mky',
+                description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
+                status: 'SUBMITTED',
+                time: '1919年8月10日',
+                received_by: '',
+                tags: ['生物制沼', '沼气动力学'],
+                audit_by: ''
+            }
+        ]
     }
 })
+
+const { mode, is_card, articleList } = toRefs(props)
+
+const articleListRef = ref(articleList.value)
 
 const currentSettings = reactive({
     statusVisible: true,
@@ -53,80 +146,10 @@ const modeSettings = {
     }
 }
 
-const TestArticleList = reactive([
-    {
-        articleId: 'a3F8c0krY3df',
-        title: '关于沼气动力学的研究',
-        contributor: '田所浩二',
-        userId: '114514',
-        organization: '下北泽中学',
-        grade: '五年级',
-        mentor: '野兽先辈',
-        description: '这是一篇关于沼气动力学的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者田所浩二潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为沼气动力学的研究提供了新的思路和方法。',
-        status: 'PUBLISHED',
-        isPublic: true,
-        time: '1919年8月10日',
-        received_by: '下北泽日报',
-        tags: ['生物制沼', '沼气动力学']
-    },
-    {
-        articleId: 'a2',
-        title: '关于生物制沼的研究',
-        contributor: 'Mizuro_',
-        userId: '1919810',
-        organization: '下北泽师范大学',
-        grade: '大三',
-        mentor: 'mky',
-        description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
-        status: 'PUBLISHED',
-        time: '1919年8月10日',
-        received_by: '',
-        tags: ['生物制沼', '沼气动力学']
-    },
-    {
-        articleId: 'a3',
-        title: '关于生物制沼的研究',
-        contributor: 'Mizuro_',
-        userId: '1919810',
-        organization: '下北泽师范大学',
-        grade: '大三',
-        mentor: 'mky',
-        description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
-        status: 'FAIL_AUDITED',
-        time: '1919年8月10日',
-        received_by: '',
-        tags: ['生物制沼', '沼气动力学']
-    },
-    {
-        articleId: 'a4',
-        title: '关于生物制沼的研究',
-        contributor: 'Mizuro_',
-        userId: '1919810',
-        organization: '下北泽师范大学',
-        grade: '大三',
-        mentor: 'mky',
-        description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
-        status: 'ROUGH',
-        time: '1919年8月10日',
-        received_by: '',
-        tags: ['生物制沼', '沼气动力学']
-    },
-    {
-        articleId: 'a5',
-        title: '关于生物制沼的研究',
-        contributor: 'Mizuro_',
-        userId: '1919810',
-        organization: '下北泽师范大学',
-        grade: '大三',
-        mentor: 'mky',
-        description: '这是一篇关于生物制沼的研究论文，主要研究了沼气的产生、利用和储存等方面的问题。这世间有关沼气动力学的资料少之又少，为了填补这方面的空缺，作者Mizuro_潜心研究，不断付出实践，终于完成了这一篇大作。在这篇论文中，作者详细介绍了沼气的产生原理、利用方法和储存技术，为生物制沼的研究提供了新的思路和方法。',
-        status: 'SUBMITTED',
-        time: '1919年8月10日',
-        received_by: '',
-        tags: ['生物制沼', '沼气动力学'],
-        audit_by: ''
-    }
-])
+onMounted(() => {
+    Object.assign(currentSettings, modeSettings[props.mode])
+})
+console.log(articleListRef.value.length)
 
 const pageInfo = reactive({
     currentPage: 1,
@@ -140,6 +163,15 @@ const pageInfo = reactive({
     margin: 10px;
     border-radius: 10px;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.not-card-info {
+    border-top: 1px solid #f0f0f0;
+    border-bottom: 1px solid #f0f0f0;
+}
+
+.not-card-info:hover {
+    background-color: #fafafa;
 }
 
 .article_list_pagination {
