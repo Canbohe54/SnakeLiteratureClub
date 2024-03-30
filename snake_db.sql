e1df13f990eed0bb121547cb384255b4875b843d
X <2060162656@qq.com>
2024-03-30 22:54:11 +0800

    更新数据库结构

--- snake_db.sql
+++ snake_db.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : snake_db
+ Source Server         : MySQL8
  Source Server Type    : MySQL
- Source Server Version : 80100
- Source Host           : localhost:3306
+ Source Server Version : 80032
+ Source Host           : localhost:3308
  Source Schema         : snake_db
 
  Target Server Type    : MySQL
- Target Server Version : 80100
+ Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 30/03/2024 14:09:12
+ Date: 30/03/2024 22:52:48
 */
 
 SET NAMES utf8mb4;
@@ -39,8 +39,22 @@ CREATE TABLE `article`  (
   `author_org` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `author_grade` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `audited_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for article_likes
+-- ----------------------------
+DROP TABLE IF EXISTS `article_likes`;
+CREATE TABLE `article_likes`  (
+  `like_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '点赞信息ID',
+  `article_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '点赞对象id',
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '点赞人ID',
+  `status` int(0) NULL DEFAULT 0 COMMENT '0 取消 1 点赞',
+  PRIMARY KEY (`like_id`) USING BTREE,
+  UNIQUE INDEX `agdkey`(`user_id`, `article_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '点赞记录表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for comments
@@ -109,6 +123,28 @@ CREATE TABLE `grades`  (
   CONSTRAINT `fk_grade_article_art_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_grade_user_expert_id` FOREIGN KEY (`expert_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group
+-- ----------------------------
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  INDEX `fk_group_user_user_id`(`user_id`) USING BTREE,
+  CONSTRAINT `fk_group_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for likes_views_count
+-- ----------------------------
+DROP TABLE IF EXISTS `likes_views_count`;
+CREATE TABLE `likes_views_count`  (
+  `article_id` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '稿件id',
+  `like_count` int(0) NULL DEFAULT NULL COMMENT '点赞数',
+  `view_count` int(0) NULL DEFAULT NULL COMMENT '浏览量',
+  PRIMARY KEY (`article_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for message
