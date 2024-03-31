cb10e8de221955ff84c61169792d2524af417aa5
Mizuiro_ <674.zfer@gmail.com>
2024-03-31 18:51:38 +0800

    - new article status update

---
+++ application/literatureClub/src/main/java/com/snach/literatureclub/common/ArticlePublishStatus.java
@@ -0,0 +1,11 @@
+package com.snach.literatureclub.common;
+
+public enum ArticlePublishStatus {
+    PUBLIC,
+    UNDER_REVIEW,
+    UNDER_RECORD,
+    POST_RECORD,
+    POSTED,
+    FAILED_REVIEW,
+    FAILED_RECORD
+}
